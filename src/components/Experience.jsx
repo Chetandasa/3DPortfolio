25137323a6c9215bd5b20cd6aa13a8d3afec6b5b
Your Name <chetandasa2814@gmail.com>
2023-04-22 21:39:01 +0530

    3D Portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -61,10 +61,10 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
+          Work
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          What I have done so far.
+          What I have Done so far.
         </h2>
       </motion.div>
 
