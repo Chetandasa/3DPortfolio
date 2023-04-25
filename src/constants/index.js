6b4c8c46b752de835cbc5d98756009d973015764
Your Name <chetandasa2814@gmail.com>
2023-04-25 22:40:17 +0530

    3D Portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -200,7 +200,7 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Properties-webiste",
       description:
         "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
       tags: [
