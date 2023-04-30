2e66ac020c22d284961553e346a7394c7b4d2d28
Your Name <chetandasa2814@gmail.com>
2023-04-30 22:32:32 +0530

    ..

--- src/constants/index.js
+++ src/constants/index.js
@@ -119,13 +119,12 @@ import {
   const experiences = [
     {
       title: "React.js Developer",
-      company_name: "Starbucks",
+      company_name: "Real EState",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "March 2023",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
