99f4e52890667b1796a2d1dd7acbdb666dd18087
Your Name <chetandasa2814@gmail.com>
2023-05-02 23:29:20 +0530

    ..

--- src/constants/index.js
+++ src/constants/index.js
@@ -278,6 +278,27 @@ import {
       image: carrent,
     },
 
+    {
+      name: "Netflix Clone",
+      description:
+        "AI-Based Web Application using OpanAI's GPT model Sammarize is the Best Application. How does this work you can take any URL of link the Articles and past its over here AI will Generate the Summary for You",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+    },
+
   ];
   
   export { services, technologies, experiences, testimonials, projects };
