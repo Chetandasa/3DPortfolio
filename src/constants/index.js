a43a574628ca2b1cfadf311999be7edff5a0ca03
Your Name <chetandasa2814@gmail.com>
2023-05-23 23:34:16 +0530

    ..

--- src/constants/index.js
+++ src/constants/index.js
@@ -232,7 +232,7 @@ import {
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/Chetandasa",
+      source_code_link: "",
     },
     {
       name: "Trip Guide",
