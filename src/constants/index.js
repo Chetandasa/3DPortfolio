464ab77931d32139a079ce4c9244d89a565a67c0
Your Name <chetandasa2814@gmail.com>
2023-06-15 23:14:31 +0530

    ..

--- src/constants/index.js
+++ src/constants/index.js
@@ -218,7 +218,7 @@ import {
     {
       name: "CarHub",
       description:
-        "Web application car rent",
+        "Web application car rents",
       tags: [
         {
           name: "react.js",
